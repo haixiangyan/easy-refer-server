69fd12fdfdb8eb3810339b254900b531b64f2be8
Haixiang <haixiang6123@gmail.com>
2020-04-16 23:21:09 -0700

    尝试向 travis 添加 mysql

--- tests/unit/routes/auth.spec.ts
+++ tests/unit/routes/auth.spec.ts
@@ -1,6 +1,23 @@
+import request from 'supertest'
+import db from '@/models/db'
+import app from '@/app'
+import {initMockDB} from '../../../mocks/dbObjects'
+
 describe('auth', () => {
-  it('简单测试一下', () => {
-    expect(1 + 1).toBe(2)
-    expect(2 + 1).toBe(3)
+  beforeAll(async () => {
+    await db.sync({force: true})
+    await initMockDB()
+  })
+  describe('/login', () => {
+    it('成功登录', async () => {
+      const res: request.Response = await request(app)
+        .post('/api/auth/login')
+        .send({
+          email: 'user1@mail.com',
+          password: '123456'
+        })
+      expect(res.status).toEqual(200)
+      expect(res.body).toHaveProperty('token')
+    })
   })
 })
