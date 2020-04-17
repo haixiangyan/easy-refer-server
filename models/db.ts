61a1a7e3e668cf88f1fd7df993f804185d20567e
Haixiang <haixiang6123@gmail.com>
2020-04-17 00:00:52 -0700

    不检查 DB_PASSWORD

--- models/db.ts
+++ models/db.ts
@@ -10,7 +10,7 @@ import {parseEnv} from '@/utils/config'
 const initDB = () => {
   const {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD} = process.env
 
-  if (!DB_HOST || !DB_PORT || !DB_NAME || !DB_USER || !DB_PASSWORD) {
+  if (!DB_HOST || !DB_PORT || !DB_NAME || !DB_USER) {
     throw new Error('环境变量不存在')
   }
 
